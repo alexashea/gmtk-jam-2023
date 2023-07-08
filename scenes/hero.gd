5abeaeed8c94135329329f8b52fb32754bc5163d
Alexa Shea <alexa.v.shea@gmail.com>
2023-07-07 21:59:02 -0400

    fix: fix arrow key movement

--- scenes/hero.gd
+++ scenes/hero.gd
@@ -18,21 +18,22 @@ func start(start_position: Vector2, goal_position: Vector2) -> void:
 	set_movement_target(goal_position)
 
 
-func move(vel: Vector2) -> void:
+func _process(delta: float) -> void:
+	var velocity := Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
-		vel += Vector2.UP
+		velocity += Vector2.UP
 	if Input.is_action_pressed("ui_down"):
-		vel += Vector2.DOWN
+		velocity += Vector2.DOWN
 	if Input.is_action_pressed("ui_left"):
-		vel += Vector2.LEFT
+		velocity += Vector2.LEFT
 	if Input.is_action_pressed("ui_right"):
-		vel += Vector2.RIGHT
+		velocity += Vector2.RIGHT
 
-	if not vel == Vector2.ZERO:
-		vel = vel.normalized()
+	if not velocity == Vector2.ZERO:
+		velocity = velocity.normalized()
 		$AnimatedSprite2D.play("walk")
 		$AnimatedSprite2D.set_flip_h(velocity.x < 0)
-		velocity = vel * speed
+		position += velocity * speed * delta
 	else:
 		$AnimatedSprite2D.stop()
 
@@ -55,7 +56,8 @@ func _physics_process(delta: float) -> void:
 
 	var new_velocity: Vector2 = next_path_position - current_agent_position
 	print("velocity: %s, %s" % [new_velocity.x, new_velocity.y])
-	move(new_velocity)
+	new_velocity = new_velocity.normalized()
+	new_velocity = new_velocity * speed
 
 	velocity = new_velocity
 	move_and_slide()
