70afe84d3d2e872e4412e3de1df366003da2a473
Alexa Shea <alexa.v.shea@gmail.com>
2023-07-08 11:58:49 -0400

    chore: register collisions, get hero and mob fighting, and have hero open treasure chest

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 
 var window_size: Vector2
-var treasure_tile := Vector2i(4, 3)
-var treasure_position := (Vector2(18.5, 3.5)) * 16
+var treasure_position := (Vector2(18.5, 4)) * 16
+var open_treasure_atlas_id := Vector2i(3, 19)
+var closed_treasure_atlas_id := Vector2i(2, 19)
 
 
 func _ready() -> void:
@@ -18,6 +19,8 @@ func _ready() -> void:
 			if i == 0:
 				dungeon_size.y += tile_room.get_used_rect().size.y * tile_room.cell_quadrant_size
 
+	set_treasure_tile(true)
+
 #	var dungeon_scale: int = min(window_size.x/dungeon_size.x, window_size.y/dungeon_size.y)
 #	print(window_size.x, ",", dungeon_size.x, ",", window_size.y, ",", dungeon_size.y, ",", dungeon_scale)
 #	$Dungeon.apply_scale(Vector2(dungeon_scale, dungeon_scale))
@@ -31,6 +34,25 @@ func _ready() -> void:
 	hero_start_position.y += dungeon_size.y / 2 + 16
 	$Hero.start(hero_start_position, treasure_position)
 
+	var mob_start_position := (Vector2(18.5, 8.5)) * 16
+	$Mob.start(mob_start_position, hero_start_position)
+	$MobTimer.start()
+
+
+func set_treasure_tile(closed: bool) -> void:
+	var atlas_id: Vector2i = closed_treasure_atlas_id if closed else open_treasure_atlas_id
+	var treasure_tile := Vector2i(3, -4)
+
+	$Dungeon/Treasury.erase_cell(1, treasure_tile)
+	$Dungeon/Treasury.set_cell(1, treasure_tile, $Dungeon/Treasury.tile_set.get_source_id(0), atlas_id)
+
+	# TODO: play theft sfx
+
 
 func _on_hero_found_treasure():
-	pass
+	set_treasure_tile(false)
+
+
+func _on_mob_timer_timeout():
+	$Mob.set_movement_target($Hero.global_position)
+	$MobTimer.start()
--- scenes/mob.gd
+++ scenes/mob.gd
@@ -1,15 +1,11 @@
-class_name Hero
+class_name Mob
 extends CharacterBody2D
 
 
 signal found_treasure
-signal hit_mob
 
 var speed: int = 20
-var has_treasure: bool = false
 var is_fighting: bool = false
-var exit_position: Vector2
-var treasure_position: Vector2
 
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
@@ -20,8 +16,6 @@ func _ready() -> void:
 
 
 func start(start_position: Vector2, goal_position: Vector2) -> void:
-	exit_position = start_position
-	treasure_position = goal_position
 	position = start_position
 	await get_tree().physics_frame
 	set_movement_target(goal_position)
@@ -31,25 +25,17 @@ func set_movement_target(movement_target: Vector2) -> void:
 	navigation_agent.target_position = movement_target
 
 
-func get_manual_movement() -> Vector2:
-	var manual_velocity := Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		manual_velocity += Vector2.UP
-	if Input.is_action_pressed("ui_down"):
-		manual_velocity += Vector2.DOWN
-	if Input.is_action_pressed("ui_left"):
-		manual_velocity += Vector2.LEFT
-	if Input.is_action_pressed("ui_right"):
-		manual_velocity += Vector2.RIGHT
-	return manual_velocity
-
-
 func set_walk_animation() -> void:
 	if not velocity == Vector2.ZERO:
 		$AnimatedSprite2D.play("walk")
 		$AnimatedSprite2D.set_flip_h(velocity.x < 0)
 	else:
-		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play("idle")
+
+
+func fight() -> void:
+	is_fighting = true
+	$AnimatedSprite2D.play("attack")
 
 
 func _physics_process(_delta: float) -> void:
@@ -57,33 +43,15 @@ func _physics_process(_delta: float) -> void:
 		return
 
 	if navigation_agent.is_navigation_finished():
-		if not has_treasure:
-			has_treasure = true
-			set_movement_target(exit_position)
-			found_treasure.emit()
-
-		velocity = get_manual_movement().normalized() * speed
-		set_walk_animation()
-		move_and_slide()
+		velocity = Vector2.ZERO
 		return
 
 	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 
 	var new_velocity: Vector2 = next_path_position - current_agent_position
-	new_velocity += get_manual_movement()
 	new_velocity = new_velocity.normalized() * speed
 
 	velocity = new_velocity
 	set_walk_animation()
 	move_and_slide()
-
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	var mob := area.get_parent() as Mob
-	if mob:
-		hit_mob.emit()
-		$AnimatedSprite2D.play("attack")
-		$AnimatedSprite2D.set_flip_h(global_position.x > mob.global_position.x)
-		is_fighting = true
-		mob.fight()
