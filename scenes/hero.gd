2032498be71491f88baf1d31e7c3e53e8d488ea3
Alexa Shea <alexa.v.shea@gmail.com>
2023-07-08 14:02:52 -0400

    feat: finish fight logic

--- scenes/hero.gd
+++ scenes/hero.gd
@@ -3,13 +3,22 @@ extends CharacterBody2D
 
 
 signal found_treasure
+signal escaped
 signal hit_mob
+signal died
 
-var speed: int = 20
+var move_speed: int = 20
 var has_treasure: bool = false
-var is_fighting: bool = false
 var exit_position: Vector2
 var treasure_position: Vector2
+
+var is_fighting: bool = false
+var attack_speed: int = 4
+var attack_time: float = max(11 - attack_speed, 1) / 4
+var max_health: int = 50
+var health: int = 50
+var attack_strength: int = 10
+var attacking_mob: Mob
 
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
@@ -17,6 +26,11 @@ var treasure_position: Vector2
 func _ready() -> void:
 	navigation_agent.path_desired_distance = 1.5
 	navigation_agent.target_desired_distance = 1.5
+
+
+func _process(_delta: float) -> void:
+	if health > 0 and not $AnimatedSprite2D.is_playing():
+		$AnimatedSprite2D.play("idle")
 
 
 func start(start_position: Vector2, goal_position: Vector2) -> void:
@@ -52,8 +66,17 @@ func set_walk_animation() -> void:
 		$AnimatedSprite2D.stop()
 
 
+func take_damage(damage: int) -> void:
+	health -= damage
+	print("hero health %s" % health)
+	if health <= 0:
+		print("hero died")
+		$AnimatedSprite2D.play("death")
+		died.emit()
+
+
 func _physics_process(_delta: float) -> void:
-	if is_fighting:
+	if is_fighting or health <= 0:
 		return
 
 	if navigation_agent.is_navigation_finished():
@@ -61,8 +84,10 @@ func _physics_process(_delta: float) -> void:
 			has_treasure = true
 			set_movement_target(exit_position)
 			found_treasure.emit()
+		else:
+			escaped.emit()
 
-		velocity = get_manual_movement().normalized() * speed
+		velocity = get_manual_movement().normalized() * move_speed
 		set_walk_animation()
 		move_and_slide()
 		return
@@ -72,7 +97,7 @@ func _physics_process(_delta: float) -> void:
 
 	var new_velocity: Vector2 = next_path_position - current_agent_position
 	new_velocity += get_manual_movement()
-	new_velocity = new_velocity.normalized() * speed
+	new_velocity = new_velocity.normalized() * move_speed
 
 	velocity = new_velocity
 	set_walk_animation()
@@ -81,9 +106,24 @@ func _physics_process(_delta: float) -> void:
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	var mob := area.get_parent() as Mob
-	if mob:
+	if mob and mob.health > 0:
 		hit_mob.emit()
-		$AnimatedSprite2D.play("attack")
+		$AnimatedSprite2D.play("idle")
 		$AnimatedSprite2D.set_flip_h(global_position.x > mob.global_position.x)
 		is_fighting = true
-		mob.fight()
+		attacking_mob = mob
+		attacking_mob.fight(self)
+		$AttackTimer.start(attack_time)
+		attacking_mob.died.connect(_on_mob_died)
+
+
+func _on_attack_timer_timeout() -> void:
+	if health > 0 && attacking_mob.health > 0:
+		$AnimatedSprite2D.play("attack")
+		attacking_mob.take_damage(attack_strength)
+		$AttackTimer.start(attack_time)
+
+
+func _on_mob_died() -> void:
+	is_fighting = false
+	attacking_mob.died.disconnect(_on_mob_died)
--- scenes/mob.gd
+++ scenes/mob.gd
@@ -3,9 +3,18 @@ extends CharacterBody2D
 
 
 signal found_treasure
+signal died
 
-var speed: int = 20
+var home_location: Vector2
+var move_speed: int = 20
+
 var is_fighting: bool = false
+var attack_speed: int = 5
+var attack_time: float = max(11 - attack_speed, 1) / 4
+var max_health: int = 40
+var health: int = 40
+var attack_strength: int = 5
+var hero: Hero
 
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
@@ -15,8 +24,14 @@ func _ready() -> void:
 	navigation_agent.target_desired_distance = 1.5
 
 
+func _process(_delta: float) -> void:
+	if health > 0 and not $AnimatedSprite2D.is_playing():
+		$AnimatedSprite2D.play("idle")
+
+
 func start(start_position: Vector2, goal_position: Vector2) -> void:
 	position = start_position
+	home_location = start_position
 	await get_tree().physics_frame
 	set_movement_target(goal_position)
 
@@ -33,13 +48,24 @@ func set_walk_animation() -> void:
 		$AnimatedSprite2D.play("idle")
 
 
-func fight() -> void:
+func fight(hero: Hero) -> void:
+	$AnimatedSprite2D.play("idle")
 	is_fighting = true
-	$AnimatedSprite2D.play("attack")
+	self.hero = hero
+	$AttackTimer.start(attack_time)
+
+
+func take_damage(damage: int) -> void:
+	health -= damage
+	print("mob health %s" % health)
+	if health <= 0:
+		print("mob died")
+		$AnimatedSprite2D.play("death")
+		died.emit()
 
 
 func _physics_process(_delta: float) -> void:
-	if is_fighting:
+	if is_fighting or health <= 0:
 		return
 
 	if navigation_agent.is_navigation_finished():
@@ -50,8 +76,15 @@ func _physics_process(_delta: float) -> void:
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 
 	var new_velocity: Vector2 = next_path_position - current_agent_position
-	new_velocity = new_velocity.normalized() * speed
+	new_velocity = new_velocity.normalized() * move_speed
 
 	velocity = new_velocity
 	set_walk_animation()
 	move_and_slide()
+
+
+func _on_attack_timer_timeout():
+	if health > 0 && hero.health > 0:
+		$AnimatedSprite2D.play("attack")
+		hero.take_damage(attack_strength)
+		$AttackTimer.start(attack_time)
